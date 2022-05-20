4ed8fb0e2daf7bedf831e7ac3ed396af75d6a2cf
Jeevanantham N <jeevanantham3006@gmail.com>
2022-05-20 11:40:00 +0530

    Employee Component
    Post Method Creation

--- AwardSystem/src/app/admin/employee/add-employee/add-employee.component.ts
+++ AwardSystem/src/app/admin/employee/add-employee/add-employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Department } from 'Models/Department';
+import { Designation } from 'Models/Designation';
+import { Organisation } from 'Models/Organisation';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-add-employee',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sharedService:SharedService) { }
+
+  Employee : any = {
+    id : 0,
+    aceid : '',
+    firstName : '',
+    lastName : '',
+    email : '',
+    dob : '',
+    organisationId : 0,
+    departmentId : 0,
+    designationId : 0,
+    reportingPersonId : 0,
+    hrID : 0,
+    password : '',
+    isActive : true,
+  }
+
+  organisations: Organisation[] = [];
+  departments: Department[] = [];
+  designations: Designation[] = [];
+
+  SelectOrg: any = 0;
+  SelectDep: any = 0;
+
+  endpoint = "Organisation";
+  endpoint1 = "Employee";
 
   ngOnInit(): void {
+    this.sharedService.getAll(this.endpoint).subscribe(data=>{
+      this.organisations=data;
+      console.log(this.organisations);
+    });
   }
+
+  onSelectDep(){
+    this.sharedService.getDepartmentByOrganisation(this.SelectOrg).subscribe(data=>{
+      this.departments = data;
+      console.log(this.departments);
+    });
+   }
+
+   onSelectDes(){
+    this.sharedService.getDesignationByDepartment(this.SelectDep).subscribe(data=>{
+      this.designations = data;
+      console.log(this.designations);
+    });
+   }
+
+   OnSubmit(){
+    this.sharedService.add(this.endpoint1,this.Employee).subscribe(data=>{
+      console.log(data);
+    });
+  }
+  
 
 }
