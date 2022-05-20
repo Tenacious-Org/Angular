d16046cf70e24a8a0d0f3adff870b1224230c673
Jeevanantham N <jeevanantham3006@gmail.com>
2022-05-20 12:52:33 +0530

    Employee Components

--- AwardSystem/Models/Employee.ts
+++ AwardSystem/Models/Employee.ts
@@ -6,6 +6,7 @@ export class Employee
     lastName : string = '';
     email : string = '';
     dob : string = '';
+    image : string = '';
     organisationId : number = 0;
     departmentId : number = 0;
     designationId : number = 0;
--- AwardSystem/src/app/admin/employee/add-employee/add-employee.component.ts
+++ AwardSystem/src/app/admin/employee/add-employee/add-employee.component.ts
@@ -13,6 +13,10 @@ export class AddEmployeeComponent implements OnInit {
 
   constructor(private sharedService:SharedService) { }
 
+  imageError = "";
+  isImageSaved: boolean = false;
+  cardImageBase64 = "";
+
   Employee : any = {
     id : 0,
     aceid : '',
@@ -20,6 +24,8 @@ export class AddEmployeeComponent implements OnInit {
     lastName : '',
     email : '',
     dob : '',
+    Image:'',
+    imageString : this.cardImageBase64,
     organisationId : 0,
     departmentId : 0,
     designationId : 0,
@@ -28,6 +34,8 @@ export class AddEmployeeComponent implements OnInit {
     password : '',
     isActive : true,
   }
+
+  
 
   organisations: Organisation[] = [];
   departments: Department[] = [];
@@ -65,6 +73,48 @@ export class AddEmployeeComponent implements OnInit {
       console.log(data);
     });
   }
-  
+
+
+
+  ImageConversion(fileInput:any){
+    this.imageError = "";
+    if (fileInput.target.files && fileInput.target.files[0]) {
+
+      const max_size = 20971520;
+      const allowed_types = ['image/png', 'image/jpeg'];
+
+      if (fileInput.target.files[0].size > max_size) {
+        this.imageError =
+          'Maximum size allowed is ' + max_size / 1000 + 'Mb';
+
+        return false;
+      }
+      console.log(fileInput.target.files[0].type)
+
+      if (!allowed_types.includes(fileInput.target.files[0].type)) {
+        this.imageError = 'Only Images are allowed ( JPG | PNG )';
+        return false;
+      }
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const image = new Image();
+        image.src = e.target.result;
+        image.onload = rs => {
+
+          const imgBase64Path = e.target.result;
+          
+          this.cardImageBase64 = imgBase64Path;
+          this.cardImageBase64= this.cardImageBase64.replace("data:image/png;base64,", "");
+          this.cardImageBase64= this.cardImageBase64.replace("data:image/jpg;base64,", "");
+          this.cardImageBase64= this.cardImageBase64.replace("data:image/jpeg;base64,", "");
+          this.Employee.imageString=this.cardImageBase64;
+          this.isImageSaved = true;
+        }
+      };
+
+      reader.readAsDataURL(fileInput.target.files[0]);
+    } return false
+  }
+
 
 }
