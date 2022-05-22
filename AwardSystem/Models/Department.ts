ac47ab10631955a66fb751e3011dd2ca20797067
Jeevanantham N <jeevanantham3006@gmail.com>
2022-05-22 15:32:42 +0530

    Update Department.ts

--- AwardSystem/Models/Department.ts
+++ AwardSystem/Models/Department.ts
@@ -2,7 +2,7 @@ import { Organisation } from "./Organisation";
 
 export class Department
 {
-    id : number = 0;
+    id = 0;
     departmentName : string = '';
     organisationId : number = 0;
     isActive : boolean = true;
