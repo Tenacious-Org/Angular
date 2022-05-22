a5fb8d8fd32797eb545c79cddfe06e2555256169
Jeevanantham N <jeevanantham3006@gmail.com>
2022-05-22 15:33:48 +0530

    Create DepartmentsVM.ts
    Added Typescript file for getting the Foreign key values in Name

---
+++ AwardSystem/ViewModels/DepartmentsVM.ts
@@ -0,0 +1,11 @@
+export class DepartmentVM
+{
+    id = 0;
+    departmentName = '';
+    organisationName = '';
+    isActive = true;
+    addedBy = 0;
+    addedOn = Date.now;
+    updatedBy = 0;
+    updatedOn = Date.now;
+}
