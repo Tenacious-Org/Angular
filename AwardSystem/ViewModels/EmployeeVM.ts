4f55fe0759c30fcc4ebda083f0563f1a6483bf48
Jeevanantham N <jeevanantham3006@gmail.com>
2022-05-24 16:24:25 +0530

    Create EmployeeVM.ts

---
+++ AwardSystem/ViewModels/EmployeeVM.ts
@@ -0,0 +1,17 @@
+export class EmployeeVM
+{
+    id = 0;
+    firstName = '';
+    lastName = '';
+    email = '';
+    dob = '';
+    image = '';
+    organisationName = '';
+    departmentName = '';
+    designationName = '';
+    reportingPersonName = '';
+    hRName = '';
+    password = '';
+    isActive = true;
+
+}
