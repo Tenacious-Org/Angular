e276594bceeb16c592a3934ea1357169bac35b3b
Jeevanantham N <jeevanantham3006@gmail.com>
2022-05-24 16:00:44 +0530

    Create Designation.ts

---
+++ AwardSystem/ViewModels/Designation.ts
@@ -0,0 +1,11 @@
+export class DesignationVM
+{
+    id = 0;
+    designationName = '';
+    departmentName = '';
+    isActive = true;
+    addedBy = 0;
+    addedOn = Date.now;
+    updatedBy = 0;
+    updatedOn = Date.now;
+}
