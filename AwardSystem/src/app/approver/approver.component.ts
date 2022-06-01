1a718c129e5c738e6b8bdd793499c14a6c95e956
aravinthdexterous <aravinthdexterous@gmail.com>
2022-06-01 15:02:56 +0530

    Search - updated

--- AwardSystem/src/app/approver/approver.component.ts
+++ AwardSystem/src/app/approver/approver.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AwardService } from 'src/app/award.service';
 
 @Component({
   selector: 'app-approver',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./approver.component.css']
 })
 export class ApproverComponent implements OnInit {
+  pageId=3;
+  employeeId=5;
+  totalLength: any;
+  page: number = 1;
+  data:any;
 
-  constructor() { }
+  constructor(private awardService:AwardService) { }
 
   ngOnInit(): void {
+    this.awardService.getAwardsList(this.pageId,this.employeeId).subscribe(data=>
+    {
+        this.data = data;
+        this.totalLength=data;
+        console.log(this.data)
+    });
   }
 
 }
