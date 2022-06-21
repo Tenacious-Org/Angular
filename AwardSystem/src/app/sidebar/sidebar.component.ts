db722cf79996f77ce2ddc0188794e181a18ead50
Madujith M A <madujith.mc18@bitsathy.ac.in>
2022-06-21 14:31:43 +0530

    sidebar hiding

--- AwardSystem/src/app/sidebar/sidebar.component.ts
+++ AwardSystem/src/app/sidebar/sidebar.component.ts
@@ -1,9 +1,41 @@
+import { trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  animateChild } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  animations: [
+    trigger('ngIfAnimation', [
+      transition(':enter, :leave', [
+        query('@*', animateChild())
+      ])
+    ]),
+    trigger('easeInOut', [
+      transition('void => *', [
+          style({
+              opacity: 0
+          }),
+          animate("500ms ease-in", style({
+              opacity: 1
+          }))
+      ]),
+      transition('* => void', [
+          style({
+              opacity: 1
+          }),
+          animate("500ms ease-in", style({
+              opacity: 0
+          }))
+        ])
+      ])
+  ]
 })
 export class SidebarComponent implements OnInit {
   admin=1;
@@ -17,5 +49,11 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {
     this.role=this.publisher;
   }
+  isShowDivIf = false;
+  
+  toggleDisplayDivIf() {
+    this.isShowDivIf = !this.isShowDivIf;
+  }
+
 
 }
