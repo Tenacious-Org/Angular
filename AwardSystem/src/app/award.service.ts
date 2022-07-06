9b3422c9c64568bd8fd37d240fa3df880ea35b1b
ajaybharathi0820 <ajaybharathik@gmail.com>
2022-07-06 10:16:14 +0530

    token added in headers

--- AwardSystem/src/app/award.service.ts
+++ AwardSystem/src/app/award.service.ts
@@ -16,40 +16,34 @@ export class AwardService {
 
   // Get Awards --> pageId 0=Homepage , 1=myawards, 2= requesterList , 3= approverList, 4=HR List .
   getAwardsList(pageId:any,employeeId:any):Observable<any>{
-    return this.http.get<any>(this.apiurl+`GetAwardsList?pageId=${pageId}&employeeId=${employeeId}`)
+    return this.http.get<any>(this.apiurl+`GetAwardsList?pageId=${pageId}&employeeId=${employeeId}`,{ headers: this.headers })
   }
 
   // Get Award By id
   getAwardById(id:any):Observable<any>
   {
-    return this.http.get<any>(this.apiurl+`GetAwardById?id=${id}`)
+    return this.http.get<any>(this.apiurl+`GetAwardById?id=${id}`,{ headers: this.headers })
   }
 
   //Approval of the request
   approval(data:any,id:any): Observable<any>
   {
-    const headers = new HttpHeaders();
-    headers.append('Content-Type', 'application/json; charset=utf-8');
-    return this.http.put<any>(this.apiurl+`Approval?id=${id}`,data,{headers});
+    return this.http.put<any>(this.apiurl+`Approval?id=${id}`,data,{ headers: this.headers });
   }
 
   // Add comments - Awardservices
   addComment(data:any): Observable<any>
   {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json; charset=utf-8');
-    return this.http.post<any>(this.apiurl +'AddComment', data)
+    return this.http.post<any>(this.apiurl +'AddComment', data,{ headers: this.headers })
   }
   addRequest(data:any,id:any): Observable<any>
   {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json; charset=utf-8');
-    return this.http.post<any>(this.apiurl +`RaiseRequest?id=${id}`, data)
+    return this.http.post<any>(this.apiurl +`RaiseRequest?id=${id}`, data,{ headers: this.headers })
   }
 
   getComments(awardId:any):Observable<any>
   {
-    return this.http.get<any>(this.apiurl+`GetComments?awardId=${awardId}`)
+    return this.http.get<any>(this.apiurl+`GetComments?awardId=${awardId}`,{ headers: this.headers })
   }
 
 }
--- AwardSystem/src/app/shared.service.ts
+++ AwardSystem/src/app/shared.service.ts
@@ -17,72 +17,71 @@ export class SharedService {
 //GetAll method
   getAll(endpoint:string):Observable<any>
   {
-    return this.http.get<any>(this.apiurl + endpoint +'/GetAll')
+    return this.http.get<any>(this.apiurl + endpoint +'/GetAll',{ headers: this.headers })
   }
 
 //Get By ID method
   getById(endpoint:any,id:any)
   {
-    return this.http.get<any>(this.apiurl + endpoint+`/GetById?id=${id}`)
+    return this.http.get<any>(this.apiurl + endpoint+`/GetById?id=${id}`,{ headers: this.headers })
   }
 
 //create method
   add(endpoint:any,data:any):Observable<any>
   {
-    const headers = new Headers();
-    headers.append('Content-Type', 'application/json; charset=utf-8');
-    return this.http.post<any>(this.apiurl + endpoint+'/Create', data)
+    
+    return this.http.post<any>(this.apiurl + endpoint+'/Create', data,{ headers: this.headers })
   }
 
 //Update method
   edit(endpoint:any,data:any):Observable<any>
   {
-    return this.http.put<any>(this.apiurl + endpoint +`/Update`, data)
+    return this.http.put<any>(this.apiurl + endpoint +`/Update`, data,{ headers: this.headers })
   }
 
 //Disable method
 disable(endpoint:any,id:any):Observable<string>{
-  return this.http.put<any>(this.apiurl+endpoint+`/Disable?id=${id}`,Object)
+  return this.http.put<any>(this.apiurl+endpoint+`/Disable?id=${id}`,Object,{ headers: this.headers })
 }
 
 //Get Department By Organisation ID
   getDepartmentByOrganisation(id:any):Observable<any>
   {
-    return this.http.get<any>(this.apiurl + `Department/GetDepartmentsByOrganisationId?id=${id}`)
+    return this.http.get<any>(this.apiurl + `Department/GetDepartmentsByOrganisationId?id=${id}`,{ headers: this.headers })
   }
 
 //Get Designation By Department ID
   getDesignationByDepartment(id:any):Observable<any>
   {
-    return this.http.get<any>(this.apiurl + `Designation/GetDesignationsByDepartmentId?id=${id}`)
+    return this.http.get<any>(this.apiurl + `Designation/GetDesignationsByDepartmentId?id=${id}`,{ headers: this.headers })
   }
 
 //Get Employee By Department ID
   getEmployeeByDepartment(id:any):Observable<any>
   {
-    return this.http.get<any>(this.apiurl + `Employee/GetEmployeeByDepartment?id=${id}`)
+    return this.http.get<any>(this.apiurl + `Employee/GetEmployeeByDepartment?id=${id}`,{ headers: this.headers })
   }
 
   //Get ReportingPerson By Department ID
   getReportingPersonByDepartment(id:any):Observable<any>
   {
-    return this.http.get<any>(this.apiurl + `Employee/GetReportingPersonByDepartment?id=${id}`)
+    return this.http.get<any>(this.apiurl + `Employee/GetReportingPersonByDepartment?id=${id}`,{ headers: this.headers })
   }
 
   //Get HR By Department ID
   getHrByDepartment(id:any):Observable<any>
   {
-    return this.http.get<any>(this.apiurl + `Employee/GetHrByDepartment?id=${id}`)
+    return this.http.get<any>(this.apiurl + `Employee/GetHrByDepartment?id=${id}`,{ headers: this.headers })
   }
 
 //Get Requested Award List
   getRequestedAwardList(id:number):Observable<any>
   {
-    return this.http.get<any>(this.apiurl +`Award/GetRequestedAwardsList?id=${id}`)
+    return this.http.get<any>(this.apiurl +`Award/GetRequestedAwardsList?id=${id}`,{ headers: this.headers })
   }
   getEmployeeByRequester(id:any):Observable<any>
   {
-    return this.http.get<any>(this.apiurl+`Employee/GetEmployeeByRequesterId?id=${id}`)
+    return this.http.get<any>(this.apiurl+`Employee/GetEmployeeByRequesterId?id=${id}`,{ headers: this.headers })
 
   }
 
