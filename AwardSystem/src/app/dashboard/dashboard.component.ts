22c39c5f989fa055eb5db957d6ad6a26669c9a4c
Jeevanantham N <jeevanantham3006@gmail.com>
2022-07-15 23:58:46 +0530

    Update dashboard.component.ts

--- AwardSystem/src/app/dashboard/dashboard.component.ts
+++ AwardSystem/src/app/dashboard/dashboard.component.ts
@@ -7,7 +7,6 @@ import { SharedService } from 'src/app/shared.service';
 import { Employee } from 'Models/Employee';
 import { AwardType } from 'Models/AwardType';
 import { AwardService } from '../award.service';
-import { dashboard } from 'Models/Dashboard';
 import { BaseChartDirective } from 'ng2-charts';
 import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { FormControl, FormGroup } from '@angular/forms';
@@ -35,8 +34,8 @@ export class DashboardComponent implements OnInit {
   res = false;
 
   // Pie
-  public pieChartOptions: ChartOptions<'pie'> = {
-    responsive: false,
+  public pieChartOptions: ChartOptions<'bar'> = {
+        responsive: false,
   };
   public pieChartLabels = this.award;
   public pieChartDatasets = [ {
@@ -128,6 +127,14 @@ export class DashboardComponent implements OnInit {
       this.awardData = data;
     });
   }
+
+  onSelectDepCascade(){
+    console.log("Lokkiiiiiiiiiii: ", this.SelectOrg)
+    this.sharedService.getDepartmentByOrganisation(this.SelectOrg).subscribe(data=>{
+      this.departments = data;
+      console.log(this.departments);
+    });
+   }
 
   Pie():void{
     this.sharedService.getallwinner().subscribe( (res) =>
@@ -310,10 +317,104 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  onSelectDep(){
+
+    this.sharedService.getallwinDeptwise(this.SelectDep).subscribe( res => {
+
+      let tres = true;
+      this.res = tres;
+
+      //converting api values into list
+      let d = []
+      let d1:string[][] = []
+      for(var i of res){
+        for(let key in i){
+          let value = i[key];
+          d.push(value)
+        }
+        d1.push(d)
+        d = []          
+      }
+      console.log(d1)
+
+      //setting into calculate a total count in dictionary
+      var dict:any = {}
+      for(var a of d1){
+        for(var b of a){
+          var new_item = b
+          dict[new_item] = dict.hasOwnProperty(new_item)? ++dict[new_item] : 1;
+        }
+      }
+      console.log(dict)
+
+      //Setting Organisation into a list
+      var org:any = []
+      const search = (targetElement : string) => (arrElement : string) => arrElement === targetElement;
+      for(var x of d1){
+          if(org.some(search(x[0]))){
+            continue
+          }
+          else{
+            org.push(x[0])
+          }
+      }
+      this.org = org
+      console.log(this.org)
+
+      //Setting Awards into a list
+      var award:any = []
+      for(var x of d1){
+          if(award.some(search(x[2]))){
+            continue
+          }
+          else{
+            award.push(x[2])
+          }
+      }
+      
+      console.log(this.award)
+
+      //setting award values into a list
+      var orgcnt:any = []
+      for(var j of org){
+        for(var h of Object.keys(dict)){
+          if(j == h){
+            orgcnt.push(dict[h])
+          }
+        }
+      }
+      this.orgcnt = orgcnt
+      this.pieChartLabels = award
+      console.log("Organisation :",this.orgcnt)
+
+      //setting award values into a list
+      var awdcnt:any = []
+      for(var j of award){
+        for(var k of Object.keys(dict)){
+          if(j == k){
+            awdcnt.push(dict[k])
+          }
+        }
+      }
+      console.log("Awards :",awdcnt)
+
+      // Dictionary Creation and uploaded it to a list
+      var temp:any = {}
+      temp["data"] = awdcnt
+      console.log("Temp : ",temp)
+      var temp1 = []
+      temp1.push(temp)
+      this.pieChartDatasets = temp1
+
+    })
+
+  }
+
 
   
 
   onSelectorg() {
+    this.onSelectDepCascade()
     this.sharedService.getallwinOrgwise(this.SelectOrg).subscribe(res => {
 
       let tres = true;
@@ -402,7 +503,7 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  Apply(orgid: number, awdid: number, fdate:Date, tdate:Date){
+  Apply(orgid: number, deptid: number, awdid: number, fdate:Date, tdate:Date){
     console.log("org: ", orgid)
     console.log("awd: ", awdid)
     console.log("fdate: ", this.fromdate)
@@ -410,7 +511,7 @@ export class DashboardComponent implements OnInit {
 
     //Filter By Date Range
     if(orgid != 0 && awdid != 0 && fdate != new Date("0001-04-15") && tdate != new Date("0001-04-29")){
-      this.sharedService.getDateWise(this.SelectOrg, this.SelectAward, this.fromdate, this.todate).subscribe(res => {
+      this.sharedService.getDateWise(this.SelectOrg, this.SelectDep, this.SelectAward, this.fromdate, this.todate).subscribe(res => {
 
         //converting api values into list
       let d = []
