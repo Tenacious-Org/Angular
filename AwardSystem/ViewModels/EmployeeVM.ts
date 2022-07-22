fa5642a3cfa61344aee258ffa69b160f108215b1
Karthickraja Selvaraj <65103626+karthickraja1215@users.noreply.github.com>
2022-07-22 12:23:29 +0530

    Merge branch 'VidhyaPriya' of https://github.com/Tenacious-Org/Angular into VidhyaPriya

--- AwardSystem/ViewModels/EmployeeVM.ts
+++ AwardSystem/ViewModels/EmployeeVM.ts
@@ -1,29 +1,29 @@
 export class EmployeeVM
 {
-    id = 0;
-    aceid = 0;
-    firstName = '';
-    lastName = '';
-    fullName='';
-    email = '';
-    gender = '';
-    dob = '';
-    image = '';
-    organisationId = '';
-    departmentId = '';
-    designationId = '';
-    organisationName = '';
-    departmentName = '';
-    designationName = '';
-    reportingPersonId='';
-    hrId = '';
-    reportingPersonName = '';
-    hRName = '';
-    password = '';
-    isActive = true;
-    addedBy = 0;
-    addedOn = Date.now;
-    updatedBy = 0;
+    id :number =0;
+    aceid : number = 0;
+    firstName : string = '';
+    lastName : string = '';
+    fullName : string ='';
+    email : string = '';
+    gender : string = '';
+    dob : string = '';
+    image : string = '';
+    organisationId : number = 0;
+    departmentId : number = 0;
+    designationId : number = 0;
+    organisationName : string = '';
+    departmentName : string = '';
+    designationName : string = '';
+    reportingPersonId : number= 0;
+    hrId : number = 0;
+    reportingPersonName : string = '';
+    hRName : string = '';
+    password : string = '';
+    isActive : boolean = true;
+    addedBy : number = 0;
+    addedOn  = Date.now;
+    updatedBy : number = 0;
     updatedOn = Date.now;
 
 
