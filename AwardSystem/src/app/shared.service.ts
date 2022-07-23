cc80be71e7a02748981997fe710311ca49fbafc9
Jeevanantham N <jeevanantham3006@gmail.com>
2022-07-23 11:17:48 +0530

    Update shared.service.ts

--- AwardSystem/src/app/shared.service.ts
+++ AwardSystem/src/app/shared.service.ts
@@ -6,7 +6,7 @@ import { AuthenticationService } from './authentication.service';
   providedIn: 'root'
 })
 export class SharedService {
-  readonly apiurl = "http://172.24.209.186/DedSec/";
+  readonly apiurl = "https://localhost:7275/";
   constructor(private http:HttpClient) { }
 
   public headers = new HttpHeaders({
