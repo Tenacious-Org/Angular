870a606840a204d68b96999e0ed21432d9fd64b2
Jeevanantham N <jeevanantham3006@gmail.com>
2022-07-25 09:21:32 +0530

    Updated IIS Server Link

--- AwardSystem/src/app/shared.service.ts
+++ AwardSystem/src/app/shared.service.ts
@@ -6,7 +6,7 @@ import { AuthenticationService } from './authentication.service';
   providedIn: 'root'
 })
 export class SharedService {
-  readonly apiurl = "https://localhost:7275/";
+  readonly apiurl = "http://172.24.209.186/DedSec/";
   constructor(private http:HttpClient) { }
 
   public headers = new HttpHeaders({
